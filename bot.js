bf8d3a41b1f5e2778c1503d63b98ce420a123570
Glitch (hello-express) <none>
2020-03-28 08:30:10 +0000

    🌜🍃 Checkpoint ./bot.js:3756271/1285 ./komutlar/chatlog.js:3756271/1219

--- bot.js
+++ bot.js
@@ -198,3 +198,44 @@ client.on("message", async function(msg) {
 });
 
 // CAPSLOCK \\
+
+// CHAT LOG \\
+client.on("messageDelete", async message => {
+  if (message.author.bot) return;
+
+  var yapan = message.author;
+
+  var kanal = await db.fetch(`chatlog_${message.guild.id}`);
+  if (!kanal) return;
+  var kanalbul = message.guild.channels.find("name", kanal);
+
+  const chatembed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`Bir Mesaj Silindi!`, yapan.avatarURL)
+    .addField("Kullanıcı Tag", yapan.tag, true)
+    .addField("ID", yapan.id, true)
+    .addField("Silinen Mesaj", "```" + message.content + "```")
+    .setThumbnail(yapan.avatarURL);
+  kanalbul.send(chatembed);
+});
+
+client.on("messageUpdate", async (oldMsg, newMsg) => {
+  if (oldMsg.author.bot) return;
+
+  var yapan = oldMsg.author;
+
+  var kanal = await db.fetch(`chatlog_${oldMsg.guild.id}`);
+  if (!kanal) return;
+  var kanalbul = oldMsg.guild.channels.find("name", kanal);
+
+  const chatembed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`Bir Mesaj Düzenlendi!`, yapan.avatarURL)
+    .addField("Kullanıcı Tag", yapan.tag, true)
+    .addField("ID", yapan.id, true)
+    .addField("Eski Mesaj", "```" + oldMsg.content + "```")
+    .addField("Yeni Mesaj", "```" + newMsg.content + "```")
+    .setThumbnail(yapan.avatarURL);
+  kanalbul.send(chatembed);
+});
+// CHAT LOG \\
